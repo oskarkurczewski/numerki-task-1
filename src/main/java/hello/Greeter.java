bc23bc1054aca5717c393714d8c7885f2fabc0c5
229935 <229935@edu.p.lodz.pl>
2021-03-16 19:51:11 +0100

    First commit

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Greeter {
+    public String sayHello() {
+        return "Numerki 2021 :)";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println("\n\n");
+        System.out.println(greeter.sayHello());
+        System.out.println("\n\n");
+    }
+}
