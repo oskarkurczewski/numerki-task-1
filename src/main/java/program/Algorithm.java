4579491565deda17a3ee0abf1d6fdb0b67396972
229935 <229935@edu.p.lodz.pl>
2021-03-16 22:49:02 +0100

    unify formatting

--- src/main/java/program/Algorithm.java
+++ src/main/java/program/Algorithm.java
@@ -3,24 +3,23 @@ package program;
 import java.util.Scanner;
 
 public class Algorithm {
-    public void euler(double a, double b, boolean m, int number) {
+    public void euler(double left, double right, boolean estimation, double f) {
         Scanner scanner = new Scanner(System.in);
         int i = 0, iterations = Integer.MAX_VALUE;
         double x, epsilon = -1.0;
         
-        if (m==false)
-        {
-            scanner.nextDouble();
+        if (estimation == true) {
+            System.out.println("");
+            epsilon = scanner.nextDouble();
         }
-        else
-        {
-            scanner.nextInt();
+        else {
+            System.out.println("");
+            iterations = scanner.nextInt();
         }
-        while ((Math.abs(b-a)>epsilon) && (i<iterations) && (a!=b))
-        {
-            x =b- f(b) *(b-a)/(f(b)-f(a));
-            a=b;
-            b=x;
+        while ((Math.abs(right - left) > epsilon) && (i < iterations) && (left != right)) {
+            x = right - f(right) * (right-left) / (f(right)-f(left));
+            left = right;
+            right = x;
             i++;
         }
     }
